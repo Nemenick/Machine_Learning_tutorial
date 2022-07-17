97b5baf69eaaab6da46eadca099f55d0bb396b44
Nemenick <giovanni.messuti.98@gmail.com>
2022-07-17 13:38:56 +0200

    predictior

--- ML_Tutorial_1.py
+++ ML_Tutorial_1.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 from tensorflow import keras
 # from tensorflow.keras.layers import Dense, Flatten, Conv2D
 # from tensorflow.keras import Model
-import numpy
+import numpy as np
 import matplotlib.pyplot as plt
 import pandas
 """
@@ -18,24 +18,40 @@ Create a `Sequential` model and add a Dense layer as the first layer.
     model.add(tf.keras.layers.Dense(32))
     model.output_shape
 """
+# Classifico pari vs dispari
+
 print("TensorFlow version:", tf.__version__)
-(x_train,y_train) , (x_test,y_test) = keras.datasets.mnist.load_data()
+(x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
 print(x_train.shape, y_train.shape, type(x_train))
 # plt.matshow(x_test[3])
 # plt.show()
-(x_train,x_test) = (x_train.reshape(60000,28*28),x_test.reshape(len(x_test),28*28))
+(y_train, y_test) = (y_train % 2, y_test % 2)
+
+(x_train, x_test) = (x_train.reshape(60000, 28*28), x_test.reshape(len(x_test), 28*28))
 print(x_train.shape, x_test.shape)
 
 model = tf.keras.models.Sequential([
-    tf.keras.layers.Dense(20, input_shape=(784,), activation="relu"),
-    # 20 è la dim dell'output, prende in input un vettore di 784 length, ma non stabilisco quanti vettori gli do
-    tf.keras.layers.Dense(10, activation="softmax")
+    tf.keras.layers.Dense(16, input_shape=(784,), activation="relu"),
+    # 16 è la dim dell'output, prende in input un vettore di 784 length, ma non stabilisco quanti vettori gli do
+    tf.keras.layers.Dense(1, activation="sigmoid")
     # non ho bisgno di specificare più input
 ])
 
-# model.compile(
-#     optimizer="adam",
-#     loss="ehhhhhh"
-# )
+model.compile(
+    optimizer="adam",
+    loss="binary_crossentropy",
+    metrics=['accuracy']
+)
 
+storia = model.fit(x_train, y_train, batch_size=32, epochs=1)
+
+print(storia.history)
+
+predizione = np.array(model.predict(x_test))
+predizione = predizione.reshape(len(predizione),)
+print("predict", predizione)
+dizionario = {"predizione": predizione, "test_label": y_test}
+print(dizionario)
+dizionario = pandas.DataFrame.from_dict(dizionario)
+dizionario.to_csv("Eccolo.csv")
 
